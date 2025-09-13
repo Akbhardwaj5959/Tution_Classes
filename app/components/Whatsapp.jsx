95587fda8b5a6e705f4b78cb12adacb28b09c9fa
Akbhardwaj5959 <riyajay31@gmail.com>
2025-09-13 17:36:31 +0530

    immage-change

--- app/components/Whatsapp.jsx
+++ app/components/Whatsapp.jsx
@@ -11,7 +11,7 @@ export default function WhatsappFloat() {
       aria-label="Chat on WhatsApp"
     >
       <Image
-        src="/whatsappicon.jpg"
+        src="/whatsapp.jpg"
         alt="WhatsApp"
         width={48}
         height={48}
